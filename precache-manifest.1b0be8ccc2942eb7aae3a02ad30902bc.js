e2e3a19398955e8f9eb446a4d3bbf29291368d46
iigonzoii <a.gonzalez418@gmail.com>
2021-07-20 20:06:19 -0600

    Updates

---
+++ precache-manifest.1b0be8ccc2942eb7aae3a02ad30902bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e810863012ae944d3a7b",
+    "url": "/todomvc/static/css/main.6d415543.chunk.css"
+  },
+  {
+    "revision": "e810863012ae944d3a7b",
+    "url": "/todomvc/static/js/main.e8108630.chunk.js"
+  },
+  {
+    "revision": "79cc1f24d3a36d41dabb",
+    "url": "/todomvc/static/js/runtime~main.79cc1f24.js"
+  },
+  {
+    "revision": "f938f36385ee20624b7c",
+    "url": "/todomvc/static/css/2.cbc8c9d2.chunk.css"
+  },
+  {
+    "revision": "f938f36385ee20624b7c",
+    "url": "/todomvc/static/js/2.f938f363.chunk.js"
+  },
+  {
+    "revision": "1f3d418fe11dbcc834186e5e1841999b",
+    "url": "/todomvc/index.html"
+  }
+];
